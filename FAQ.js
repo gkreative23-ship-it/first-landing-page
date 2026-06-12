5a70fb60261dd408647884fa465b2108a96e427e
gkreative23-ship-it <gkreative23@gmail.com>
2026-06-12 18:04:01 +0100

    added a little FAQ to the website.

---
+++ FAQ.js
@@ -0,0 +1,22 @@
+const question=document.querySelector('.faq-question');
+const answer=document.querySelector('.faq-answer');
+
+const hide=document.querySelector('.hideButton');
+
+let isAnswerShowing=false;
+
+function showAnswer(){
+    if(isAnswerShowing === false){
+        answer.innerHTML=`<button onclick="hideAnswer()" class="hideButton">Depends on the needs of the clients, as well as the number of revisions. But it usually takes four to seven working days</button>`;
+
+        isAnswerShowing= true;
+    } else{
+        answer.innerHTML="";
+
+        isAnswerShowing= false;
+    }
+}
+
+function hideAnswer(){
+    hide.innerHTML='';
+}
