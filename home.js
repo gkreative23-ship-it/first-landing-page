7bb4105024730d9a23f6a04d08129dc8af7d678a
gkreative23-ship-it <gkreative23@gmail.com>
2026-06-12 13:14:21 +0100

    addded a javascript function to the 'start your journey' button and made more adjustments to the color scheme of the website

--- home.js
+++ home.js
@@ -1,17 +1,3 @@
-const button= document.querySelector('.mainbtn');
-
-
-button.addEventListener('click', ()=>{
+function moveToForm(){
     window.location.href="form.html"
-});
-
-const batton=document.getElementById('blog');
-
-batton.addEventListener('click', ()=>{
-    window.location.href="blog.html"
-});
-
-const open=document.getElementById('blog');
-open.addEventListener('click', ()=>{
-    window.location.href="blog.html"
-});
+}
